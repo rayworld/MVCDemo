2ee08716948815ea77051d34ef32e1a132761024
rayworld <casp@sina.com>
2018-12-16 15:17:21 +0800

    sadf

--- MVCDemo/Models/TRoles.cs
+++ MVCDemo/Models/TRoles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,13 @@ namespace MVCDemo.Models
     {
         public int ID { get; set; }
 
+        [Display(Name="角色名称")]
+        [Required]
+        [MaxLength=10]
         public string Name { get; set; }
 
+        [Display(Name = "角色描述")]
+        [MaxLength=50]
         public string Desc { get; set; }
 
         public virtual ICollection<TUserRoles> SysUserRole { get; set; }
--- MVCDemo/Models/TUsers.cs
+++ MVCDemo/Models/TUsers.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCDemo.Models
 {
@@ -6,8 +7,13 @@ namespace MVCDemo.Models
     {
         public int ID { get; set; }
 
+        [Display(Name = "用户名称")]
+        [Required]
+        [MaxLength=10]
         public string Name { get; set; }
 
+        [Display(Name = "密码")]
+        [Required]
         public string Password { get; set; }
 
         public virtual ICollection<TUserRoles> SysUserRole { get; set; }
